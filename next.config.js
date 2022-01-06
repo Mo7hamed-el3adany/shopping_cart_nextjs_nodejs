3e08d8276d5458a5c67f3f5763bc795fa5bb093a
El-Sadany32 <mohammedelsadany32@gmail.com>
2022-01-06 16:54:18 +0200

    first commit

---
+++ next.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    env: {
+        "BASE_URL": "https://ecommerce-32.herokuapp.com",
+        "MONGODB_URL": "mongodb+srv://elsadany32:01550148448abc@cluster0.l2f80.mongodb.net/myFirstDatabase",
+        "ACCESS_TOKEN_SECRET": "J{psMWhjtxE[wq&*;+'Ka,!Z:jWFXALL>J{wVe@uA_-MaB$6z{4x[9j}pzWE;c=S<c!<Az2A];3G+ESB`vs3[49'TG@EnyY,8nha-$+{)RDhTQ-9mV/^mT3D>*'",
+        "REFRESH_TOKEN_SECRET": "GPV`yyDzsu^~4$,~$B,D:k``r}tj^,?2%wW62t=)nCN{CaT4dAYa?Qjyr>&FZN[}ux252z5~:F>?e$YR:-Xq{#!SzAUu#()XjSVt9WmFa(yu>",
+        "PAYPAL_CLIENT_ID": "Adwa-Cd2xgUzFzMWTmYZA6c9Dc3iOFaflUdMwGg-fwabey4ko0SGbUELixsWbWjQ3UFHg9u9_bcKe9_e",
+        "CLOUD_UPDATE_PRESET": "nextjs-app",
+        "CLOUD_NAME": "djamk74m7",
+        "CLOUD_API": "https://api.cloudinary.com/v1_1/djamk74m7/image/upload"
+    }
+}
